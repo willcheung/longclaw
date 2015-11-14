677018186454cff8efee8c3ffb6f1cb64dec35f2
Will Cheung <wcheung@comprehend.com>
2015-11-13 23:18:30 -0800

    Added start and end dates to email.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,11 +7,13 @@ class UserMailer < ApplicationMailer
     mail(to: @user.email, subject: "Welcome to ContextSmith!")
   end
 
-  def beta_teaser_email(user, data)
+  def beta_teaser_email(user, data, start_date, end_date)
     # curl -H "Content-Type: application/json" --data @/Users/wcheung/Downloads/contextsmith_project_details.json http://localhost:3000/users/64eb67f6-3ed1-4678-84ab-618d348cdf3a/send_beta_teaser_email.json
 
     @user = user
     @data = data
+    @start_date = start_date
+    @end_date = end_date
 
     track user: user
     mail(to: @user.email, subject: "Projects Recap by ContextSmith")
