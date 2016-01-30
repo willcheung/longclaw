a144eea6055847dbf9e3c5ae54aa03dcf807a189
Will Cheung <willycheung@gmail.com>
2016-01-30 00:18:34 -0800

    Dates on homepage are now dynamic.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,13 @@ class HomeController < ApplicationController
   layout 'empty', only: 'access_denied'
 
   def index
-    #activities_today = Project.visible_to(current_user.organization_id, current_user.id).eager_load(:activities).where("activities.last_sent_date::date = ?", Time.now().strftime('%F')).group("activities.id")
-    activities_today = Project.visible_to(current_user.organization_id, current_user.id).eager_load([:activities, :account]).where("activities.last_sent_date::date = ?", '2014-11-26').group("activities.id, accounts.id")
+    date_filter =  params[:date] || Time.now.strftime('%F')
+    @date_now = Date.parse(date_filter)
+    
+    activities_today = Project.visible_to(current_user.organization_id, current_user.id).eager_load([:activities, :account]).where("activities.last_sent_date::date = ?", date_filter).group("activities.id, accounts.id")
     @projects_with_activities_today = activities_today.group_by{|e| e.activities}
 
-    @pinned_activities_today = Project.visible_to(current_user.organization_id, current_user.id).eager_load([:activities]).where("activities.is_pinned = true and activities.pinned_at::date = ?", '2014-11-26').group("activities.id")
+    @pinned_activities_today = Project.visible_to(current_user.organization_id, current_user.id).eager_load([:activities]).where("activities.is_pinned = true and activities.pinned_at::date = ?", date_filter).group("activities.id")
   end
 
   def access_denied
