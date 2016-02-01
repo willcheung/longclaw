44b321dd37628e76f63b3c7f19d6aea0336aab1a
Will Cheung <willycheung@gmail.com>
2016-01-31 22:12:10 -0800

    google analytics again.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,17 +20,6 @@
 /* Ahoy analytics */
 ahoy.trackAll();
 
-/* Google analytics */
-<% if ENV["RAILS_ENV"] == 'production' %>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-68686500-2', 'auto');
-  ga('send', 'pageview');
-<% end %>
-
 
 $(document).ready(function() {
   /* Activating Best In Place */
