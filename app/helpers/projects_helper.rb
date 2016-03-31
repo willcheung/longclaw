3bda37b2bea269e5f886efbb143814b93cc06a26
Will Cheung <willycheung@gmail.com>
2016-03-30 20:47:24 -0700

    Bug fix for nil not_in_current_inbox activities.

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,11 +1,9 @@
 module ProjectsHelper
 	def not_in_current_user_inbox(a)
-		a.email_messages ||= []
-
 		!(
 			a.email_messages.map{|m| m.to.map { |to| to.address }.flatten }.include?(current_user.email) or
 			a.email_messages.map{|m| m.from.map { |from| from.address }.flatten }.include?(current_user.email) or
-			a.email_messages.map{|m| m.cc.map { |cc| cc.address }.flatten }.include?(current_user.email)
+			a.email_messages.map{|m| m.cc.map.nil? ? [] : (m.cc.map { |cc| cc.address }.flatten) }.include?(current_user.email)
 		)
 	end
 end
