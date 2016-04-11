6d7f310e98f13e53a74268b021db5c64df5a24c8
Kelvin Lu <kelvin.lu.sj@gmail.com>
2016-04-11 16:23:37 -0700

    add option to display top/bottom 5 or all on customer reports, charts resize to fit data set

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -12,22 +12,35 @@ class ReportsController < ApplicationController
 
     ###### Report Data ######
     if !@projects.empty?      
-      @project_sum_activities = Project.find_include_sum_activities(true, 7*24, @projects.map(&:id))
-      # sorted low to high by num_activities
-      @project_sum_activities.sort! { |x, y| x.num_activities.to_i <=> y.num_activities.to_i }.reverse!
+      @project_all_touches = Project.find_include_sum_activities(true, 7*24, @projects.map(&:id))
+      # sorted high to low by num_activities
+      @project_all_touches.sort! { |x, y| x.num_activities.to_i <=> y.num_activities.to_i }.reverse!
+      if @project_all_touches.length > 10
+        # take first and last 5 from sorted array == most /least
+        @project_top_touches = @project_all_touches[0, 5] + @project_all_touches[-5, 5]
+      else
+        @project_top_touches = @project_all_touches
+      end
 
-      project_prev_sum_activities = Project.find_include_sum_activities(true, 7*24, 14*24, @projects.map(&:id))
-      project_chg_activities = Project.calculate_pct_from_prev(@project_sum_activities, project_prev_sum_activities)
-      # sorted low to high by pct_from_prev
-      @project_neg_chg = project_chg_activities.sort { |x, y| x.pct_from_prev.to_f <=> y.pct_from_prev.to_f }.select { |x| x.pct_from_prev < 0 }.reverse
-      @project_no_chg = project_chg_activities.select { |x| x.pct_from_prev == 0 }
-      @project_pos_chg = project_chg_activities.sort { |x, y| x.pct_from_prev.to_f <=> y.pct_from_prev.to_f }.select { |x| x.pct_from_prev > 0 }.reverse
-      puts "===="
-      project_chg_activities.each { |x| puts x.name, x.pct_from_prev }
-      puts "===="
-      puts "===="
-      @project_pos_chg.each { |x| puts x.name, x.pct_from_prev }
-      puts "===="
+      project_prev_all_touches = Project.find_include_sum_activities(true, 7*24, 14*24, @projects.map(&:id))
+      project_chg_activities = Project.calculate_pct_from_prev(@project_all_touches, project_prev_all_touches)
+      # sorted high to low by pct_from_prev
+      project_chg_activities.sort! { |x, y| x.pct_from_prev.to_f <=> y.pct_from_prev.to_f }.reverse!
+      if project_chg_activities.length > 10
+        # take first and last 5 from sorted array == most /least
+        project_chg_activities_top = project_chg_activities[0, 5] + project_chg_activities[-5, 5]
+      else
+        project_chg_activities_top = project_chg_activities
+      end
+      @project_top_chg_touches = {}
+      @project_top_chg_touches[:pos] = project_chg_activities_top.select { |x| x.pct_from_prev > 0 }
+      @project_top_chg_touches[:no] = project_chg_activities_top.select { |x| x.pct_from_prev == 0 }
+      @project_top_chg_touches[:neg] = project_chg_activities_top.select { |x| x.pct_from_prev < 0 }
+
+      @project_all_chg_touches = {}
+      @project_all_chg_touches[:pos] = project_chg_activities.select { |x| x.pct_from_prev > 0 }
+      @project_all_chg_touches[:no] = project_chg_activities.select { |x| x.pct_from_prev == 0 }
+      @project_all_chg_touches[:neg] = project_chg_activities.select { |x| x.pct_from_prev < 0 }
     end
   end
 
