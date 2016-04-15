5c554432475bfcf13df73b2abe7d6f8b3b9136a2
Kelvin Lu <kelvin.lu.sj@gmail.com>
2016-04-15 16:31:20 -0700

    start refactoring touches chart to handle filtering

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -1,2 +1,36 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+/* Tooltip */
+$('[data-toggle="tooltip"]').tooltip();
+
+$('input[name="daterange"]').daterangepicker({
+    "alwaysShowCalendars": true,
+    "cancelClass": "btn-danger",
+    "startDate": moment().subtract(7, "days").format("l"),
+    "ranges": {
+        "Today": [
+            moment().format("l"),
+            moment().format("l")
+        ],
+        "Last 7 Days": [
+            moment().subtract(7, "days").format("l"),
+            moment().format("l")
+        ],
+        "Last 14 Days": [
+            moment().subtract(14, "days").format("l"),
+            moment().format("l")
+        ],
+        "Last 30 Days": [
+            moment().subtract(30, "days").format("l"),
+            moment().format("l")
+        ],
+        "Last 90 Days": [
+            moment().subtract(90, "days").format("l"),
+            moment().format("l")
+        ],
+        "This Month": [
+            moment().date(1).format("l"),
+            moment().date(moment().daysInMonth()).format("l")
+        ]
+    }
+});
