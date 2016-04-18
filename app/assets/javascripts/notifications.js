4d5d5475c082186ed5af1ca0dd3bd2391bcc1fac
Will Cheung <willycheung@gmail.com>
2016-04-18 14:55:28 -0700

    Changed data tables sorting.

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,6 +1,43 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+$(document).ready(function() {
+    $('#notifications-table').DataTable({
+    	responsive: true,
+	    columnDefs: [
+	      { searchable: false, targets: [0,1,3,4,5]},
+	      { orderable: false, targets: [0,1,2] }
+	    ],
+	    "bPaginate": false,
+	    "order": [[ 5, "desc" ]],
+	    "dom":' <"col-sm-4 row"f><"top">rt<"col-sm-5"l><"col-sm-5"p><"bottom"i><"clear">',
+	    "language": {
+      search: "_INPUT_",
+      searchPlaceholder: "Start typing to filter list..."
+    	}
+    });
+    $('input[type=search]').attr('size', '50');
+} );
+
+$(function() {
+    $('.i-checks').iCheck({
+        checkboxClass: 'icheckbox_square-green'
+    });
+});
+
+$('input').on('ifChecked', function(event){
+  $(this).parent().parent().parent().attr("bgcolor", "#eeeeee");
+  $.ajax({url:'/notifications/'+$(this).attr('id')+'/update_is_complete'});
+  // var temp = "#best_in_place_notification_" + $(this).attr('id') + "_name"
+  // $(temp).css({'display' : 'inline-block'});
+});
+
+
+$('input').on('ifUnchecked', function(event){
+  $(this).parent().parent().parent().attr("bgcolor", "");
+  $.ajax({url:'/notifications/'+$(this).attr('id')+'/update_is_complete'});
+});
+
 jQuery(function($){
     $.datepicker.regional['ca'] = {
         dateFormat: 'yy-mm-dd',
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -29,7 +29,7 @@ class Notification < ActiveRecord::Base
 	belongs_to  :assign_to_user, :class_name => "User", foreign_key: "assign_to"
   belongs_to  :completed_by_user, :class_name => "User", foreign_key: "completed_by"
 
-  CATEGORY = { Notification: 'Notification', Action: 'Action Item', Todo: 'To-do', Risk: 'Risk', Opportunity: 'Opportunity' }
+  CATEGORY = { Notification: 'Notification', Action: 'Smart Action', Todo: 'To-do', Risk: 'Risk', Opportunity: 'Opportunity' }
 
 	def self.load(data, project, test=false)
 		notifications = []
@@ -83,7 +83,7 @@ class Notification < ActiveRecord::Base
               next
             end
 
-    	      notification = Notification.new(category: 'Action Item',
+    	      notification = Notification.new(category: 'Smart Action',
       	      	name: contextMessage.subject,
       	      	description: description,
       	        message_id: contextMessage.messageId,
