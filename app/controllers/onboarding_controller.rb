57e6ef6513235989cea1c8f7f24144137312f5c4
Will Cheung <willycheung@gmail.com>
2016-04-22 22:57:26 -0700

    added hubspot auto enrolling users.

--- app/controllers/onboarding_controller.rb
+++ app/controllers/onboarding_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class OnboardingController < ApplicationController
 	layout 'empty', except: ['tutorial']
 
@@ -16,7 +18,9 @@ class OnboardingController < ApplicationController
 
 	# Callback method
 	def confirm_projects
-		redirect_to root_path if current_user.onboarding_step == Utils::ONBOARDING[:onboarded]
+		if current_user.onboarding_step == Utils::ONBOARDING[:onboarded]
+			redirect_to root_path and return
+		end
 
 		@overlapping_projects = []
 		@new_projects = []
@@ -138,6 +142,9 @@ class OnboardingController < ApplicationController
 		
 		# Change user onboarding flag
 		current_user.update_attributes(onboarding_step: Utils::ONBOARDING[:onboarded])
+
+		# Fire hubspot event to add new user to list
+		req = Net::HTTP.get(URI("http://track.hubspot.com/v1/event?_n=000000617114&_a=2189465&email=#{current_user.email}&firstname=#{current_user.first_name}&lastname=#{current_user.last_name}"))
 	end
 
 	#########################################################################
