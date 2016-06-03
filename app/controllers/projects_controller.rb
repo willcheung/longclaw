7bccd7bed55ecf1265f5db5da3809063a6897f00
Will Cheung <willycheung@gmail.com>
2016-06-02 22:41:42 -0700

    Fixed new project nil last touch and date.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -133,7 +133,7 @@ class ProjectsController < ApplicationController
   def get_show_data
     # metrics
     @project_last_activity_date = @project.activities.where(category: "Conversation").maximum("activities.last_sent_date")
-    @project_last_touch_by = @project.activities.find_by(category: "Conversation", last_sent_date: @project_last_activity_date).from[0].personal
+    @project_last_touch_by = @project.activities.find_by(category: "Conversation", last_sent_date: @project_last_activity_date).nil? ? "--" : @project.activities.find_by(category: "Conversation", last_sent_date: @project_last_activity_date).from[0].personal
     visible_activities = @project.activities.select { |a| a.is_visible_to(current_user) }
     @project_open_tasks = @project.notifications.where(is_complete: false).select {|n| n.conversation_id.nil? || visible_activities.any? {|a| n.project_id == a.project_id && n.conversation_id == a.backend_id } } .length
 
