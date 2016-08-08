de28ac83538191de3a1b7df2ba1f77f2876f3f8a
YuYunLiu <yuyunliu118@gmail.com>
2016-08-08 14:03:37 -0700

    add encrypt to refresh token

--- db/migrate/20160808194459_add_encrypt_to_users_step2.rb
+++ db/migrate/20160808194459_add_encrypt_to_users_step2.rb
@@ -1,5 +1,6 @@
 class AddEncryptToUsersStep2 < ActiveRecord::Migration
   def change
+  	User.reset_column_information
   	User.all.each do |u|
   		if !u.plain_oauth_refresh_token.nil?
   			u.oauth_refresh_token = u.plain_oauth_refresh_token
