b1c88a1c1e18d3603c82b885c5f26a36356bb566
YuYunLiu <yuyunliu118@gmail.com>
2016-09-15 16:46:25 -0700

    redirect to setting after salesforce login

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,13 +2,13 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def salesforce
     User.from_omniauth(request.env["omniauth.auth"], current_user.organization_id)
     # SalesforceAccount.load(current_user)
-    sign_in_and_redirect current_user
+    redirect_to settings_path
   end
 
   def salesforcesandbox
     User.from_omniauth(request.env["omniauth.auth"], current_user.organization_id)
     # SalesforceAccount.load(current_user)
-    sign_in_and_redirect current_user
+    redirect_to settings_path
   end
 
 	def google_oauth2
