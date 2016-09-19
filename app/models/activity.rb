94c1ed9e52e26ef3ff61f43996bb428e39eb6809
Will Cheung <willycheung@gmail.com>
2016-09-18 23:14:46 -0700

    Fixed is_public flag getting overwritten when refreshing.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -112,7 +112,7 @@ class Activity < ActiveRecord::Base
     end
 
     insert = 'INSERT INTO "activities" ("posted_by", "project_id", "category", "title", "is_public", "backend_id", "last_sent_date", "last_sent_date_epoch", "from", "to", "cc", "email_messages", "created_at", "updated_at") VALUES'
-    on_conflict = 'ON CONFLICT (category, backend_id, project_id) DO UPDATE SET last_sent_date = EXCLUDED.last_sent_date, last_sent_date_epoch = EXCLUDED.last_sent_date_epoch, updated_at = EXCLUDED.updated_at, email_messages = EXCLUDED.email_messages, is_public = EXCLUDED.is_public'
+    on_conflict = 'ON CONFLICT (category, backend_id, project_id) DO UPDATE SET last_sent_date = EXCLUDED.last_sent_date, last_sent_date_epoch = EXCLUDED.last_sent_date_epoch, updated_at = EXCLUDED.updated_at, email_messages = EXCLUDED.email_messages'
     values = val.join(', ')
 
     if !val.empty? and save_in_db
