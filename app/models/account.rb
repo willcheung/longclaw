64ce7c107817d05d5b8f6f6387a39939777f22b9
Will Cheung <willycheung@gmail.com>
2016-10-05 22:22:16 -0700

    Fixed account delete error.

--- app/models/account.rb
+++ app/models/account.rb
@@ -38,7 +38,7 @@ class Account < ActiveRecord::Base
     belongs_to  :organization
     belongs_to  :user, foreign_key: "owner_id"
 
-    has_many :salesforce_accounts, dependent: :nullify
+    has_many :salesforce_accounts, foreign_key: "contextsmith_account_id", dependent: :nullify
 
     validates :name, presence: true, uniqueness: { scope: :organization, message: "There's already an account with the same name." }
 
