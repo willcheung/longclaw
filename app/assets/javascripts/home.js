3de13b42f586c64f1ccfd1a71ba5065d629008f1
Kelvin Lu <kelvin.lu.sj@gmail.com>
2016-10-21 15:10:05 -0700

    move iChecks/update_is_complete to _table partial, reduce repetition

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -13,26 +13,3 @@ jQuery(document).ready(function($) {
       "dom":' <"col-sm-4 row"f><"top">rt<"col-sm-5"l><"col-sm-5"p><"bottom"i><"clear">'
     } );
   });
-
-$(function() {
-    $('.i-checks').iCheck({
-        checkboxClass: 'icheckbox_square-green'
-    });
-});
-
-$('input').on('ifChecked', function(event){
-  $(this).parent().parent().parent().attr("bgcolor", "#eeeeee");
-  $(this).parent().parent().parent().css('opacity', '0.5');
-  $.ajax({url:'/notifications/'+$(this).attr('id')+'/update_is_complete'});
-
-  var target = '#notification_row_'+$(this).attr('id');
-  $(target).fadeOut();
-  
-});
-
-
-$('input').on('ifUnchecked', function(event){
-  $(this).parent().parent().parent().attr("bgcolor", "");
-  $(this).parent().parent().parent().css('opacity', '1');
-  $.ajax({url:'/notifications/'+$(this).attr('id')+'/update_is_complete'});
-});
--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -241,26 +241,3 @@ $(document).ready(function() {
     });
     $('input[type=search]').attr('size', '50');
 });
-
-$(function() {
-    $('.i-checks').iCheck({
-        checkboxClass: 'icheckbox_square-green'
-    });
-});
-
-$('input').on('ifChecked', function(event){
-  $(this).parent().parent().parent().attr("bgcolor", "#eeeeee");
-  $(this).parent().parent().parent().css('opacity', '0.5');
-  $.ajax({url:'/notifications/'+$(this).attr('id')+'/update_is_complete'});
-
-  var target = '#notification_row_'+$(this).attr('id');
-  $(target).fadeOut();
-  
-});
-
-
-$('input').on('ifUnchecked', function(event){
-  $(this).parent().parent().parent().attr("bgcolor", "");
-  $(this).parent().parent().parent().css('opacity', '1');
-  $.ajax({url:'/notifications/'+$(this).attr('id')+'/update_is_complete'});
-});
