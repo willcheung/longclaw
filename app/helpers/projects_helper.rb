2e4ff9d0d849f4e40fd9ad113adfa15271b31cbd
Kelvin Lu <kelvin.lu.sj@gmail.com>
2016-11-01 19:53:30 -0700

    make reply email link dynamic, fill in to/subject/body

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,4 +6,26 @@ module ProjectsHelper
 			a.email_messages.map{|m| m.cc.nil? ? [] : (m.cc.map { |cc| cc.address }) }.flatten.include?(current_user.email)
 		)
 	end
+
+  def last_recipients(a)
+    addresses = Set.new
+    last_msg = a.email_messages.last
+    from = last_msg.from || []
+    to = last_msg.to || []
+    cc = last_msg.cc || []
+    addresses.merge((from + to + cc).map(&:address))
+    (addresses - [current_user.email]).to_a * ","
+  end
+
+  def last_body_ref(a)
+    last_msg = a.email_messages.last
+    sent_date = Time.zone.at(last_msg.sentDate).strftime("%b %-d, %Y %I:%M %p")
+    from_hash = last_msg.from.first
+    from = ""
+    from += " \"" + from_hash.personal + "\"" if from_hash.personal
+    from += " <" + from_hash.address + ">" if from_hash.address
+    body = ""
+    body = URI.escape(last_msg.content.body, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")) unless last_msg.content.nil? || !last_msg.content.respond_to?(:body) 
+    "On " + sent_date + "," + from + " wrote:%0A%0A" + body
+  end
 end
