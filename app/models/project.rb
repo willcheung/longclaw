b05ce267f67ac53055fd2bad462891a83fb7184d
Will Cheung <willycheung@gmail.com>
2017-01-13 11:48:45 -0800

    Adding more labels to Account and Stream.

--- app/models/project.rb
+++ app/models/project.rb
@@ -95,7 +95,7 @@ class Project < ActiveRecord::Base
   validates :budgeted_hours, numericality: { only_integer: true, allow_blank: true }
 
   STATUS = ["Active", "Completed", "On Hold", "Cancelled", "Archived"]
-  CATEGORY = { Implementation: 'Implementation', Onboarding: 'Onboarding', Opportunity: 'Opportunity', Pilot: 'Pilot', Support: 'Support', Other: 'Other' }
+  CATEGORY = { Adoption: 'Adoption', Expansion: 'Expansion', Implementation: 'Implementation', Onboarding: 'Onboarding', Opportunity: 'Opportunity', Pilot: 'Pilot', Support: 'Support', Other: 'Other' }
 
   attr_accessor :num_activities_prev, :pct_from_prev
 
