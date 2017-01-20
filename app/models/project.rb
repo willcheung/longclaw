cdd6928ba13c9174316cd2df424b5718ae272675
varianluong <varianluong@gmail.com>
2017-01-19 18:11:51 -0800

    new accounts stream bug 3

--- app/models/project.rb
+++ app/models/project.rb
@@ -306,7 +306,8 @@ class Project < ActiveRecord::Base
 
     # Days Inactive
     days_inactive_setting = risk_settings.find { |rs| rs.metric == RiskSetting::METRIC[:DaysInactive] }
-    puts "************************************* days_inactive_setting *****************************************", days_inactive_setting, "<<<<"
+    puts "************************************* days_inactive_setting *****************************************"
+    print "days_inactive_setting=", days_inactive_setting , " nil?=", days_inactive_setting.nil? , "\n" 
     last_sent_date = self.activities.where.not(category: Activity::CATEGORY[:Note]).maximum(:last_sent_date)
     days_inactive = last_sent_date.nil? ? 0 : Time.current.in_time_zone(time_zone).to_date.mjd - last_sent_date.in_time_zone(time_zone).to_date.mjd
     inactivity_risk = Project.calculate_score_by_setting(days_inactive, days_inactive_setting)
