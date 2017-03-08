2e61a64ea5c9e86acb736e4b89459e1b60e3c890
Will Cheung <willycheung@gmail.com>
2017-03-07 21:02:04 -0800

    Modified competitor dashboard.

--- app/assets/javascripts/reports.js
+++ app/assets/javascripts/reports.js
@@ -14,7 +14,8 @@ $('.category_filter').chosen({ disable_search: true, allow_single_deselect: true
 $('input[name="daterange"]').daterangepicker({
     "alwaysShowCalendars": true,
     "opens": "left",
-    "startDate": moment().subtract(180, "days").format("l"),
+    "startDate": moment().subtract(1, 'year').startOf('year'), 
+    "endDate": moment().subtract(1, 'year').endOf('year'),
     "ranges": {
         "Last 30 Days": [
             moment().subtract(30, "days").format("l"),
@@ -25,12 +26,12 @@ $('input[name="daterange"]').daterangepicker({
             moment().format("l")
         ],
         "Year 2016": [
-            moment().subtract(90, "days").format("l"),
-            moment().format("l")
+            moment().subtract(1, 'year').startOf('year'), 
+            moment().subtract(1, 'year').endOf('year')
         ],
         "Year 2015": [
-            moment().subtract(90, "days").format("l"),
-            moment().format("l")
+            moment().subtract(2, 'year').startOf('year'), 
+            moment().subtract(2, 'year').endOf('year')
         ]
     }
 });
