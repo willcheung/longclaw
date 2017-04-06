cbe0c79bf4cff10b9a93eb4bb9e7302152628427
varianluong <varianluong@gmail.com>
2017-04-05 19:06:13 -0700

    Issue #640: Update Salesforce code to enable Stream#show tabs to be displayed in Salesforce embedded page

--- app/assets/javascripts/salesforce.js
+++ app/assets/javascripts/salesforce.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+jQuery(document).ready(function($) {
+});
+
+function toggleSection(toggleSectionParentDOMObj) {
+    if (toggleSectionParentDOMObj) {
+        toggleSectionParentDOMObj.find(".toggle-icon").toggleClass("fa-caret-right fa-caret-down");
+        toggleSectionParentDOMObj.next().next().toggle(400);
+    }
+};
--- app/controllers/salesforce_controller.rb
+++ app/controllers/salesforce_controller.rb
@@ -1,93 +1,115 @@
 class SalesforceController < ApplicationController
   layout "empty", only: [:index]
 
+  # For accessing Streams#show page+tabs from a Salesforce iframe "page"
   def index
     @category_param = []
     @filter_email = []
 
-    @projects = []
-    @activities_by_month = []
-    @activities_by_date = []
-    @project = Project.new
-    @isconnect = false
-
-    @actiontype = 'show'
-    @pinned_activities = []
-    @data = []
+    @is_mapped_to_CS_account = false
 
     if params[:id].nil?
       return
     else
-      # set this salesforce id to contextsmith account id
-      @salesforce_id = params[:id]
+      # Set this salesforce id to contextsmith account id and then try to find a SF account mapping
+      @salesforce_id = params[:id]  
+      sf_account = SalesforceAccount.eager_load(:account).find_by(salesforce_account_id: @salesforce_id, contextsmith_organization_id: current_user.organization_id)
+      return if sf_account.nil?  # invalid SFDC id or id cannot be found
 
-      salesforce = SalesforceAccount.eager_load(:account).find_by(salesforce_account_id: params[:id], contextsmith_organization_id: current_user.organization_id)
-
-      if salesforce.nil?
-        return
-      end
-
-      account = salesforce.account
-      if account.nil?
-        return
-      end
+      cs_account = sf_account.account
+      return if cs_account.nil?  # no CS accounts mapped to this Salesforce account
     end
+    @is_mapped_to_CS_account = true
 
-    if !params[:actiontype].nil?
-      @actiontype = params[:actiontype]
-    end
+    @actiontype = (params[:actiontype].present? && (["index", "show", "filter_timeline", "more_timeline", "pinned_tab", "tasks_tab", "insights_tab", "arg_tab"].include? params[:actiontype])) ? params[:actiontype] : 'show'
 
-    @isconnect = true
+    # check if CS account_id is valid and in the scope
+    @streams_mapped = Project.visible_to(current_user.organization_id, current_user.id).where(account_id: cs_account.id)
+    #@streams_mapped.each { |p| puts "**************** project=#{ p.name }"}
 
-    # check if id is valid and in the scope
-
-    # for now, just use test account
-    @projects = Project.includes(:activities).where(account_id: account.id)
     activities = []
-    if !@projects.empty?
-      if !params[:pid].nil?
-        @projects.each do |p|
-          if p.id == params[:pid]
-            @final_filter_user = p.all_involved_people(current_user.email)
-            activities = Activity.get_activity_by_filter(p, params)
-            @project_risk_score = p.current_risk_score(current_user.time_zone)
-            @project = p
+
+    if @streams_mapped.present?
+      if params[:pid].present?
+        @project = @streams_mapped.detect {|p| p.id == params[:pid]} || nil
+      else
+        @project = @streams_mapped[0]
+      end
+
+      return if @project.blank?
+      #puts ">>>>>>>>>>>> @project = #{@project.id}, #{@project.name}" 
+
+      # Top status
+      @project_risk_score = @project.new_risk_score(current_user.time_zone)
+      @project_open_tasks_count = @project.notifications.open.count
+
+      # Tab specific (directly copied from "projects_controller.rb")
+      @users_reverse = get_current_org_users   # get_users_reverse
+      get_show_data
+      load_timeline if ["show", "filter_timeline", "more_timeline"].include? @actiontype
+
+      if @actiontype == "show"
+        # get data for user filter
+        @final_filter_user = @project.all_involved_people(current_user.email)
+        # get data for time series filter
+        @activities_by_category_date = @project.daily_activities(current_user.time_zone).group_by { |a| a.category }
+      elsif @actiontype == "pinned_tab"
+        @pinned_activities = @project.activities.pinned.visible_to(current_user.email).includes(:comments)
+      elsif @actiontype == "tasks_tab"
+        # show every risk regardless of private conversation
+        @notifications = @project.notifications
+      elsif @actiontype == "insights_tab"
+        @risk_score_trend = @project.new_risk_score_trend(current_user.time_zone)
+
+        # Engagement Volume Chart
+        @activities_moving_avg = @project.activities_moving_average(current_user.time_zone)
+        @activities_by_category_date = @project.daily_activities_last_x_days(current_user.time_zone).group_by { |a| a.category }
+        activity_engagement = @activities_by_category_date["Conversation"].map {|c| c.num_activities }.to_a
+
+        # TODO: Generate data for Risk Volume Chart in SQL query
+        # Risk Volume Chart
+        risk_notifications = @project.notifications.risks.where(created_at: 14.days.ago.midnight..Time.current.midnight)
+        risks_by_date = Array.new(14, 0)
+        risk_notifications.each do |r|
+          # risks_by_date based on number of days since 14 days ago
+          day_index = r.created_at.to_date.mjd - 14.days.ago.midnight.to_date.mjd
+          risks_by_date[day_index] += 1
+        end
+
+        @risk_activity_engagement = []
+        risks_by_date.zip(activity_engagement).each do | a, b|
+          if b == 0
+            @risk_activity_engagement.push(0)
+          else
+            @risk_activity_engagement.push(a/b.to_f * 100)
           end
         end
-      else
-        @final_filter_user = @projects[0].all_involved_people(current_user.email)
-        activities = Activity.get_activity_by_filter(@projects[0], params)
 
-        @project_risk_score = @projects[0].current_risk_score(current_user.time_zone)
-        @project = @projects[0]
+        #Shows the total email usage report
+        @in_outbound_report = User.total_team_usage_report([@project.account.id], current_user.organization.domain)
+        @meeting_report = User.meeting_team_report([@project.account.id], @in_outbound_report['email'])
+        
+        # TODO: Modify query and method params for count_activities_by_user_flex to take project_ids instead of account_ids
+        # Most Active Contributors & Activities By Team
+        user_num_activities = User.count_activities_by_user_flex([@project.account.id], current_user.organization.domain)
+        @team_leaderboard = []
+        @activities_by_dept = Hash.new(0)
+        activities_by_dept_total = 0
+        user_num_activities.each do |u|
+          user = User.find_by_email(u.email)
+          u.email = get_full_name(user) if user
+          @team_leaderboard << u
+          dept = user.nil? || user.department.nil? ? '(unknown)' : user.department
+          @activities_by_dept[dept] += u.inbound_count + u.outbound_count
+          activities_by_dept_total += u.inbound_count + u.outbound_count
+        end
+        # Convert Activities By Team to %
+        @activities_by_dept.each { |dept, count| @activities_by_dept[dept] = (count.to_f/activities_by_dept_total*100).round(1)  }
+        # Only show top 5 for Most Active Contributors
+        @team_leaderboard = @team_leaderboard[0...5]
+      elsif @actiontype == "arg_tab" # Account Relationship Graph
+        @data = @project.activities.where(category: %w(Conversation Meeting))
       end
-      @activities_by_month = activities.select {|a| a.is_visible_to(current_user) }.group_by {|a| a.last_sent_date.strftime('%^B %Y') }
-      activities_by_date_temp = activities.select {|a| a.is_visible_to(current_user) }.group_by {|a| a.last_sent_date.strftime('%Y %m %d') }
-
-      activities_by_date_temp.each do |date, activities|
-        temp = Struct.new(:utc_milli_timestamp, :count).new
-        temp.utc_milli_timestamp = DateTime.strptime(date, '%Y %m %d').to_i * 1000
-        temp.count = activities.length
-        @activities_by_date.push(temp)
-      end
-      @activities_by_date = @activities_by_date.sort {|x, y| y.utc_milli_timestamp <=> x.utc_milli_timestamp }.reverse!
-
-      @project_last_activity_date = @project.activities.where(category: "Conversation").maximum("activities.last_sent_date")
-      project_last_touch = @project.activities.find_by(category: "Conversation", last_sent_date: @project_last_activity_date)
-      @project_last_touch_by = project_last_touch ? project_last_touch.from[0].personal : "--"
-      @project_open_risks_count = @project.notifications.open.risks.count
-      @notifications = @project.notifications.order(:is_complete, :original_due_date)
-
-      @pinned_activities = @project.activities.pinned.includes(:comments)
-      # filter out not visible items
-      @pinned_activities = @pinned_activities.select {|a| a.is_visible_to(current_user) }
-
-      @data = @project.activities.where(category: %w(Conversation Meeting))
-
-      @project_open_tasks_count = @project.notifications.open.count
-      @project_pinned_count = @project.activities.pinned.count
-
-      @users_reverse = get_current_org_users
     end
 
     if(!params[:category].nil? and !params[:category].empty?)
@@ -384,6 +406,75 @@ class SalesforceController < ApplicationController
   end
 
   private
+
+  def get_show_data
+    # metrics
+    @project_risk_score = @project.new_risk_score(current_user.time_zone)
+    @project_open_risks_count = @project.notifications.open.risks.count
+    @project_pinned_count = @project.activities.pinned.visible_to(current_user.email).count
+    @project_open_tasks_count = @project.notifications.open.count
+    project_rag_score = @project.activities.latest_rag_score.first
+
+    if project_rag_score
+      @project_rag_status = project_rag_score['rag_score']
+    end
+
+    # old metrics
+    # @project_last_activity_date = @project.activities.where.not(category: [Activity::CATEGORY[:Note], Activity::CATEGORY[:Alert]]).maximum("activities.last_sent_date")
+    # project_last_touch = @project.conversations.find_by(last_sent_date: @project_last_activity_date)
+    # @project_last_touch_by = project_last_touch ? project_last_touch.from[0].personal : "--"
+
+    # project people
+    @project_members = @project.project_members
+    project_subscribers = @project.subscribers
+    @daily_subscribers = project_subscribers.daily
+    @weekly_subscribers = project_subscribers.weekly
+    @suggested_members = @project.project_members_all.pending
+    @user_subscription = project_subscribers.where(user: current_user).take
+
+    @salesforce_base_URL = OauthUser.get_salesforce_instance_url(current_user.organization_id)
+    @clearbit_domain = @project.account.domain? ? @project.account.domain : (@project.account.contacts.present? ? @project.account.contacts.first.email.split("@").last : "")
+
+    # for merging projects, for future use
+    # @account_projects = @project.account.projects.where.not(id: @project.id).pluck(:id, :name)
+  end
+
+  def load_timeline
+    activities = @project.activities.visible_to(current_user.email).includes(:notifications, :comments)
+    # filter by categories
+    @filter_category = []
+    if params[:category].present?
+      @filter_category = params[:category].split(',')
+      activities = activities.where(category: @filter_category)
+    end
+    # filter by people
+    @filter_email = []
+    if params[:emails].present?
+      @filter_email = params[:emails].split(',')
+      # filter for Meetings/Conversations where all people participated
+      where_email_clause = @filter_email.map { |e| "\"from\" || \"to\" || \"cc\" @> '[{\"address\":\"#{e}\"}]'::jsonb" }.join(' AND ')
+      # filter for Notes written by any people included
+      users = User.where(email: @filter_email).pluck(:id)
+      where_email_clause += " OR posted_by IN ('#{users.join("','")}')" if users.present?
+      activities = activities.where(where_email_clause)
+    end
+    # filter by time
+    @filter_time = []
+    if params[:time].present?
+      @filter_time = params[:time].split(',').map(&:to_i)
+      # filter for Meetings/Notes in time range + Conversations that have at least 1 email message in time range
+      activities = activities.where("EXTRACT(EPOCH FROM last_sent_date) BETWEEN #{@filter_time[0]} AND #{@filter_time[1]} OR ((email_messages->0->>'sentDate')::integer <= #{@filter_time[1]} AND (email_messages->-1->>'sentDate')::integer >= #{@filter_time[0]} )")
+    end
+    # pagination, must be after filters to have accurate count!
+    page_size = 10
+    @page = params[:page].blank? ? 1 : params[:page].to_i
+    @last_page = activities.count <= (page_size * @page) # check whether there is another page to load
+    activities = activities.limit(page_size).offset(page_size * (@page - 1))
+    @activities_by_month = activities.group_by {|a| Time.zone.at(a.last_sent_date).strftime('%^B %Y') }
+
+    @salesforce_base_URL = OauthUser.get_salesforce_instance_url(current_user.organization_id)
+  end
+
 
   def render_service_unavailable_error(method_name)
     puts "****SFDC****: Salesforce service unavailable in SalesforceController.#{method_name}: Cannot establish a connection!"
