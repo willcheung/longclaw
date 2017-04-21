c360f0a301ca4cbabd0046914e0246cdb0ddc2c4
varianluong <varianluong@gmail.com>
2017-04-21 03:10:21 -0700

    Fix bug in chrome extension set_sfdc_user

--- app/controllers/extension_controller.rb
+++ app/controllers/extension_controller.rb
@@ -147,11 +147,11 @@ class ExtensionController < ApplicationController
 
   def set_salesforce_user
     @salesforce_user = nil
-    if current_user.admin?
+    if current_user.present? && current_user.admin?
       # try to get salesforce production. if not connect, check if it is connected to Salesforce sandbox
       @salesforce_user = OauthUser.find_by(oauth_provider: 'salesforce', organization_id: current_user.organization_id)
       #@salesforce_user = OauthUser.find_by(oauth_provider: 'salesforcesandbox', organization_id: current_user.organization_id) if @salesforce_user.nil?
-    elsif current_user.power_or_chrome_user_only?  # AND is an individual (power user or chrome user)
+    elsif current_user.present? && current_user.power_or_chrome_user_only?  # AND is an individual (power user or chrome user)
       @salesforce_user = OauthUser.find_by(oauth_provider: 'salesforce', organization_id: current_user.organization_id, user_id: current_user.id)
       #@salesforce_user = OauthUser.find_by(oauth_provider: 'salesforcesandbox', organization_id: current_user.organization_id, user_id: current_user.id) if @salesforce_user.nil?
     end
