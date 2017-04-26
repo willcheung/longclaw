96659162d501721263133bbd72325ba456a7d6e2
varianluong <varianluong@gmail.com>
2017-04-26 15:29:51 -0700

    Remove check for current_user; should have session (I shouldn't arrive at the weird, abnormal state I did during testing).

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,12 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def salesforce
-    User.from_omniauth(request.env["omniauth.auth"], current_user.organization_id, current_user.id) if current_user.present?
+    User.from_omniauth(request.env["omniauth.auth"], current_user.organization_id, current_user.id)
     #puts "****** session return_to_path: #{ session[:return_to_path] }"
     redirect_to (session.delete(:return_to_path) || root_path)
   end
 
   def salesforcesandbox
-    User.from_omniauth(request.env["omniauth.auth"], current_user.organization_id, current_user.id) if current_user.present?
+    User.from_omniauth(request.env["omniauth.auth"], current_user.organization_id, current_user.id)
     redirect_to (session.delete(:return_to_path) || root_path)
   end
 
