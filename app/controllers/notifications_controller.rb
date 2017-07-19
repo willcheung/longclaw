8e21dbead3e95e066abb90ab9514164443919217
Kelvin Lu <kelvin.lu.sj@gmail.com>
2017-07-19 15:07:46 -0700

    add a route for downloading attachments, refactor some notifications routes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -201,6 +201,10 @@ class NotificationsController < ApplicationController
     respond_to :js
   end
 
+  def download_attachment
+    render plain: 'OK'
+  end
+
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_notification
