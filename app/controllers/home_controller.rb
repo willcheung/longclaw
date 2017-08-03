405bb5ab6dedbbd4ce807d8806d5576f3aa18a79
varianluong <varianluong@gmail.com>
2017-08-03 14:53:36 -0700

    Optimization related to previous change to 'Most Open Alerts & Tasks' dashboard on Home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,17 +6,17 @@ class HomeController < ApplicationController
     @MEMBERS_LIST_LIMIT = 8 # Max number of Opportunity members to show in mouse-over tooltip
 
     # Load all projects/opportunities visible to user, belongs to user, and to which user is subscribed
-    @visible_projects = Project.visible_to(current_user.organization_id, current_user.id)
-    current_user_projects = @visible_projects.owner_of(current_user.id).select("projects.*, false AS daily, false AS weekly")
-    subscribed_projects = @visible_projects.select("project_subscribers.daily, project_subscribers.weekly").joins(:subscribers).where(project_subscribers: {user_id: current_user.id}).group("project_subscribers.daily, project_subscribers.weekly")
+    visible_projects = Project.visible_to(current_user.organization_id, current_user.id)
+    @current_user_projects = visible_projects.owner_of(current_user.id).select("projects.*, false AS daily, false AS weekly")
+    subscribed_projects = visible_projects.select("project_subscribers.daily, project_subscribers.weekly").joins(:subscribers).where(project_subscribers: {user_id: current_user.id}).group("project_subscribers.daily, project_subscribers.weekly")
 
     # Load data for the 3 dashboards at the top of page
-    project_engagement_7d = Project.count_activities_by_category(current_user_projects.pluck(:id), current_user.organization.domain, current_user.time_zone, 7.days.ago.midnight.utc, Time.current.end_of_day.utc).group_by { |p| p.id }
+    project_engagement_7d = Project.count_activities_by_category(@current_user_projects.pluck(:id), current_user.organization.domain, current_user.time_zone, 7.days.ago.midnight.utc, Time.current.end_of_day.utc).group_by { |p| p.id }
     if project_engagement_7d.blank?
       @data_left = [] and @categories = []
     else
       @data_left = project_engagement_7d.map do |pid, activities|
-        proj = current_user_projects.find { |p| p.id == pid }
+        proj = @current_user_projects.find { |p| p.id == pid }
         Hashie::Mash.new({ id: proj.id, name: proj.name, deal_size: proj.amount, close_date: proj.close_date, y: activities, total: activities.inject(0){|sum,a| sum += (a.num_activities.present? ? a.num_activities : 0)} }) if proj.present?  # else nil
       end
     end
@@ -25,7 +25,7 @@ class HomeController < ApplicationController
 
     @data_center = @data_left.reverse
 
-    open_task_counts = Project.count_tasks_per_project(current_user_projects.pluck(:id))
+    open_task_counts = Project.count_tasks_per_project(@current_user_projects.pluck(:id))
     @data_right = open_task_counts.map do |r|
       Hashie::Mash.new({ id: r.id, name: r.name, deal_size: r.amount, close_date: r.close_date, y: r.open_risks, color: 'default'})
     end
@@ -51,14 +51,14 @@ class HomeController < ApplicationController
     # end
 
     # Load notifications for "My Alerts & Tasks"  
-    project_tasks = Notification.where(project_id: current_user_projects.pluck(:id))
+    project_tasks = Notification.where(project_id: @current_user_projects.pluck(:id))
     @open_total_tasks = project_tasks.open.where("assign_to='#{current_user.id}'").sort_by{|t| t.original_due_date.blank? ? Time.at(0) : t.original_due_date }.reverse
-    # Need this to show project name and user name
-    @projects_reverse = current_user_projects.map { |p| [p.id, p.name] }.to_h
+    # Need these to show project name and user name instead of pid and uid
+    @projects_reverse = @current_user_projects.map { |p| [p.id, p.name] }.to_h
     @users_reverse = get_current_org_users
 
     # Get all projects/opportunities that user owns or to which user is subscribed
-    @projects = current_user_projects
+    @projects = @current_user_projects
     subscribed_projects.each do |p|
       pid = @projects.find {|s| s.id == p.id}
       @projects << p if pid.nil?
@@ -73,7 +73,7 @@ class HomeController < ApplicationController
       @risk_scores = Project.new_risk_score(project_ids_a, current_user.time_zone)
       @open_risk_count = Project.open_risk_count(project_ids_a)
       @days_to_close = Project.days_to_close(project_ids_a)
-      @project_days_inactive = @visible_projects.joins(:activities).where.not(activities: { category: [Activity::CATEGORY[:Note], Activity::CATEGORY[:Alert]] }).maximum("activities.last_sent_date") # get last_sent_date
+      @project_days_inactive = visible_projects.joins(:activities).where.not(activities: { category: [Activity::CATEGORY[:Note], Activity::CATEGORY[:Alert]] }).maximum("activities.last_sent_date") # get last_sent_date
       @project_days_inactive.each { |pid, last_sent_date| @project_days_inactive[pid] = Time.current.to_date.mjd - last_sent_date.in_time_zone.to_date.mjd } # convert last_sent_date to days inactive
       @next_meetings = Activity.meetings.next_week.select("project_id, min(last_sent_date) as next_meeting").where(project_id: project_ids_a).group("project_id")
       @next_meetings = Hash[@next_meetings.map { |p| [p.project_id, p.next_meeting] }]
