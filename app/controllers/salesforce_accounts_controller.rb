a6f1e9ac62344b25b7d0e6e5b8e7e63b5f69c292
varianluong <varianluong@gmail.com>
2017-09-27 02:51:37 -0700

    Remove test files and fix formatting (tab).

--- app/controllers/salesforce_accounts_controller.rb
+++ app/controllers/salesforce_accounts_controller.rb
@@ -1,6 +1,6 @@
 class SalesforceAccountsController < ApplicationController
 
-	def set_visible_salesforce_account
+  def set_visible_salesforce_account
   #   @salesforce_account = Project.visible_to(current_user.organization_id, current_user.id).find(params[:id])
   # rescue ActiveRecord::RecordNotFound
   #   redirect_to root_url, :flash => { :error => "Project not found or is private." }
