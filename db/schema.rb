5110e45991879202f45e6c6300eb32a6e1a4fbc0
Kelvin Lu <kelvin.lu.sj@gmail.com>
2017-10-05 18:54:33 -0700

    code review changes, split the blank? checks into 2 lines

--- db/schema.rb
+++ db/schema.rb
@@ -366,6 +366,7 @@ ActiveRecord::Schema.define(version: 20171002215716) do
     t.string   "salesforce_account_id",                              default: "", null: false
     t.string   "name",                                               default: "", null: false
     t.text     "description"
+    t.decimal  "amount",                    precision: 14, scale: 2
     t.boolean  "is_closed"
     t.boolean  "is_won"
     t.string   "stage_name"
@@ -375,7 +376,6 @@ ActiveRecord::Schema.define(version: 20171002215716) do
     t.uuid     "contextsmith_project_id"
     t.decimal  "probability",               precision: 5,  scale: 2
     t.decimal  "expected_revenue",          precision: 14, scale: 2
-    t.decimal  "amount",                    precision: 14, scale: 2
     t.string   "forecast_category_name"
     t.string   "owner_id"
   end
