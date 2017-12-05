23bc985afe02d78631983df6386947937ceb4b15
Kelvin Lu <kelvin.lu.sj@gmail.com>
2017-12-05 15:31:16 -0800

    fall back to email address if contact from Salesforce has last name set to '(unknown)' (fix #1442)

--- lib/utils.rb
+++ lib/utils.rb
@@ -14,7 +14,7 @@ module Utils
   def get_full_name(user)
     return "" if user.nil?
 
-    if user.first_name.blank? && user.last_name.blank?
+    if user.first_name.blank? && (user.last_name.blank? || user.last_name == '(unknown)')
       user.email
     else
       [ActionView::Base.full_sanitizer.sanitize(user.first_name), ActionView::Base.full_sanitizer.sanitize(user.last_name)].join(" ").strip
