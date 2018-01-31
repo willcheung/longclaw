02f53712262e33cc70c824a7a7b2dd6d1102a85c
varianluong <varianluong@gmail.com>
2018-01-30 23:14:30 -0800

    #1610: Fix the time filter centering (and size).

---
+++ app/models/note.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id            :integer          not null, primary key
+#  title         :string(50)       default("")
+#  note          :text             not null
+#  noteable_type :string           not null
+#  noteable_uuid :uuid             not null
+#  user_uuid     :uuid             not null
+#  is_public     :boolean          default(FALSE)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_notes_on_noteable_type  (noteable_type)
+#  index_notes_on_noteable_uuid  (noteable_uuid)
+#  index_notes_on_user_uuid      (user_uuid)
+#
+
+class Note < ActiveRecord::Base
+	belongs_to :noteable, polymorphic: true, foreign_key: "noteable_uuid"
+
+  default_scope -> { order('created_at ASC') }
+
+	belongs_to :user, foreign_key: "user_uuid"
+end
---
+++ db/migrate/20180131001302_create_notes.rb
@@ -0,0 +1,19 @@
+class CreateNotes < ActiveRecord::Migration
+  def self.up
+    create_table :notes do |t|
+      t.string :title, :limit => 50, :default => "" 
+      t.text   :note, null:false
+      # t.references :noteable, :polymorphic => true
+      t.string  :noteable_type, null:false, index:true
+      t.uuid    :noteable_uuid, null:false, index:true  # FK
+      t.uuid    :user_uuid, null:false, index:true
+      t.boolean :is_public, default:false
+
+      t.timestamps null: false
+    end
+  end
+
+  def self.down
+    drop_table :notes
+  end
+end
