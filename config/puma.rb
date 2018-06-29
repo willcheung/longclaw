374e81ba22d43624f4208c39dd0901f8008f6c8c
Will Cheung <willycheung@gmail.com>
2018-06-28 23:32:50 -0700

    AWS fixes

--- config/puma.rb
+++ config/puma.rb
@@ -14,4 +14,8 @@ on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
 
+# https://stackoverflow.com/questions/30355569/rails-application-deployed-on-elastic-beanstalk-with-puma-fails-502-errors-on
+bind "unix:///var/run/puma/my_app.sock"
+pidfile "/var/run/puma/my_app.sock"
+
 # reference: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
