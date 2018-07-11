d465b5716e011b3d86cb2aaa7eba36a7a856ac20
Will Cheung <willycheung@gmail.com>
2018-07-11 13:15:17 -0700

    tweak puma conf

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,6 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-max_thread = Integer(ENV['MAX_THREADS'] || 6)
-min_thread = Integer(ENV['MIN_THREADS'] || 6)
+max_thread = Integer(ENV['MAX_THREADS'] || 4)
+min_thread = Integer(ENV['MIN_THREADS'] || 8)
 threads min_thread, max_thread
 
 preload_app!
@@ -8,12 +8,6 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
-
-before_fork do
-  require 'puma_worker_killer'
-
-  PumaWorkerKiller.enable_rolling_restart(12 * 3600) # 12 hours in seconds
-end
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
