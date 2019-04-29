dd7a40b0a9e4a2a6e3675feb116608db267fbadb
Will Cheung <willycheung@gmail.com>
2019-04-29 16:36:00 -0700

    Adding ssl options to disable 307 for /healthcheck elb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Longclaw::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
-  config.ssl_options = {  exclude: proc { |env| env['PATH_INFO'].start_with('/healthcheck')} }
+  config.ssl_options = {  exclude: proc { |env| env['PATH_INFO'] =~ /healthcheck/} }
 
   # Set to :debug to see everything in the log.
   config.log_level = :debug
